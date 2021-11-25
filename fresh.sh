27989521e971414fd24dc9b984bd0b626594d502
Oleg Makedonsky <satoved@gmail.com>
2021-11-25 16:14:15 +0200

    Update fresh.sh

--- fresh.sh
+++ fresh.sh
@@ -31,6 +31,7 @@ $HOME/.composer/vendor/bin/valet install
 # Create a Sites directory
 # This is a default directory for macOS user accounts but doesn't comes pre-installed
 mkdir $HOME/Sites
+valet park $HOME/Sites
 
 # Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
 rm -rf $HOME/.zshrc
